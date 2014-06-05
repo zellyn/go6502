122a3735acca5c4deb1f898bde0ffbcf32aebd99
Zellyn Hunter <zellyn@gmail.com>
2014-06-05 08:10:50 -0700

    added shell script to generate most ROMS

---
+++ asm/a2as/runtest.sh
@@ -0,0 +1,5 @@
+go run a2as.go --in ../../../goapple2/source/redbook/monitor.asm --out monitor.rom --flavor redbooka --listing monitor.lst --prefix=-1
+go run a2as.go --in ../../../goapple2/source/redbook/miniasm.asm --out miniasm.rom --flavor redbooka --listing miniasm.lst --prefix=-1
+go run a2as.go --in ../../../goapple2/source/redbook/sweet16.asm --out sweet16.rom --flavor redbooka --listing sweet16.lst --prefix=-1
+go run a2as.go --in ../../../goapple2/source/redbook/fp.asm --out fp.rom --flavor redbookb --listing fp.lst --prefix=-1
+# go run a2as.go --in ../../../goapple2/source/redbook/intbasic.asm --out intbasic.rom --flavor redbook --listing intbasic.lst --prefix=-1
